d446f9f916a8895ca2e0fa9baa9661f3c0543468
Scott <sscotth@gmail.com>
2014-03-06 12:38:12 -0600

    adjust primary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'main/index'
+  get 'main/' => 'main#index'
   get 'static_pages/index'
   root 'static_pages#index'
   resources :fillups
--- spec/routing/main_routing_spec.rb
+++ spec/routing/main_routing_spec.rb
@@ -4,7 +4,7 @@ describe StaticPagesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/main/index").should route_to("main#index")
+      get("/main/").should route_to("main#index")
     end
 
   end
